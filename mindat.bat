077cd83e22877eb8305db6f35b4f1a7d92af0b3d
julienfavre42 <62103752+julienfavre42@users.noreply.github.com>
2024-03-01 09:03:37 +0100

    Update mindat.bat

--- mindat.bat
+++ mindat.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python39_64\python.exe" C:\Users\tartempion\Pictures\mindat_scrap.py
-FOR /L %%y IN (0, 1, 10) DO RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+"C:\Program Files\Python312\python.exe" C:\Users\tartempion\Pictures\mindat_scrap.py
+FOR /L %%y IN (0, 1, 5) DO RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
